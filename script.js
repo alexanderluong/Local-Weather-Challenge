19448b4780000e46638c5328b3157e83240c35ba
alexanderluong <alexander.luong@hotmail.com>
2018-02-10 15:47:40 -0500

    Add function to get geolocation.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-  console.log("test");
+  getLocation();
+  
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        // TODO: add something to do for the output
+      });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }
 });
